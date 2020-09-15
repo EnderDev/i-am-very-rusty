77f2e3d1416281a0332d579fb2d60b393c98d8eb
EnderDev <xenderpugx@gmail.com>
2020-09-15 22:42:09 +0100

    commit shit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nwelcome to the shitty number guessing game thing\n\nive generated a random number guess it bro i bet u cant\n");
+
+    let num = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("make a guess!");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let guess: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if(guess < num) {
+            println!("\ntoo small idiot\n")
+        } else if(guess > num) {
+            println!("\ntoo large idiot\n")
+        } else if(guess == num) {
+            println!("\nwow u did it\n");
+            break;
+        }
+    }
 }
